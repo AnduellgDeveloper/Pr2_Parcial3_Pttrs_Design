150357a39753c67d8eb7ef92258385d937287e7b
AnduellgDeveloper <151770378+AnduellgDeveloper@users.noreply.github.com>
2024-11-14 09:47:07 -0500

    Se crea poryecto para el parcial 3

---
+++ co.edu.uniquindio.parcial3/src/main/java/co/edu/uniquindio/parcial3/patrones_diseno/HelloController.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.parcial3.patrones_diseno;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
